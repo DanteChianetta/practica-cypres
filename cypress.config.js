8e0b07a858676b3c1d9e49ba5e93f3a43b1e068e
Dante Chianetta <dantechianetta@gmail.com>
2024-08-08 12:01:08 -0300

    attachments config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
-module.exports = defineConfig({
+module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
+    baseUrl: 'https://demoblaze.com', // Ajusta esto a tu URL base
+    video: true, // Habilita la grabación de video
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
   },
-});
+  reporter: 'cypress-mochawesome-reporter', // Ajusta el reporter a cypress-mochawesome-reporter si es necesario
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+};
